1aa6b137da370476adfdeb1f138990431c7ae666
Hussain <hussainsalah404@gmail.com>
2025-08-02 18:43:15 +0300

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -2,22 +2,16 @@ class BankAccount:
     """account balance"""
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
+
     """amount added to account balance"""
     def deposit(self, amount: float):
         self.account_balance += amount
+
     """amount removed to account balance"""
     def withdraw(self, amount: float):
         self.account_balance -= amount
+        return True
+
     """displays the account balance after removing and adding amounts"""
     def display_balance(self):
         print("Your balance is:", self.account_balance)
-
-account = BankAccount()
-
-depo = float(input("Deposited: "))
-draw = float(input("Withdraw: "))
-account.deposit(depo)
-account.withdraw(draw)
-
-account.display_balance()
-
