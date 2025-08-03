21e01e670d86efc4d973bf69288dd8a4983279cc
HussainSalah404 <hussainsalah404@gmail.com>
2025-08-03 17:20:33 +0300

    added unittest for SimpleCalculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,30 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Set up the SimpleCalculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_addition(self):
+        """Test the addition method."""
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+    
+    def test_subtraction(self):
+        """Test the subtraction method."""
+        self.assertEqual(self.calc.subtract(1, 2), -1)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+    
+    def test_multiplyion(self):
+        self.assertEqual(self.calc.multiply(5, 5), 25)
+        self.assertEqual(self.calc.multiply(1, 2), 2)
+    
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(15, 2), 7.5)
+        self.assertEqual(self.calc.divide(10, 0), None)
+         
+
+if __name__ == '__main__':
+        unittest.main()
